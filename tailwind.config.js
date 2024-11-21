6d7a6f30456393819d04ef3490369fabdfa28ca4
harpreet <20126185@tafe.wa.edu.au>
2024-11-21 14:29:02 +0800

    feat : adding tailwind to my project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html',
+    './about.html',
+    './contact.html',
+    './project.html',
+    './projects.html',
+    './resume.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
