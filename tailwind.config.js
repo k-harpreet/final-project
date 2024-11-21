bb17f1e8bbaa1c17f3f8e3f43be10774f64f872b
harpreet <20126185@tafe.wa.edu.au>
2024-11-21 23:25:14 +0800

    fix(home page):fixing the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
     './projects.html',
     './resume.html',
     './src/**/*.html',],
-  theme: {
+  theme: {screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
